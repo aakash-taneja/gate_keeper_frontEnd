6bcbb937821f4a5a1fd7c5d6bfed9c1fc40155c8
unknown <aakash-taneja>
2021-06-05 18:09:10 +0530

    adding login page

---
+++ src/components/accountBox/index.jsx
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { LoginForm } from "./loginForm";
+import { motion } from "framer-motion";
+import { AccountContext } from "./accountContext";
+import { SignupForm } from "./signupForm";
+
+const BoxContainer = styled.div`
+  width: 580px;
+  min-height: 550px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 19px;
+  background-color: #fff;
+  box-shadow: 10px 10px 30px -1px rgba(0,0,0,0.1);
+-webkit-box-shadow: 10px 10px 30px -1px rgba(0,0,0,0.1);
+-moz-box-shadow: 10px 10px 30px -1px rgba(0,0,0,0.1);
+  position: relative;
+  overflow: hidden;
+
+`;
+
+const TopContainer = styled.div`
+  width: 100%;
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 0 1.8em;
+  padding-bottom: 5em;
+`;
+
+const BackDrop = styled(motion.div)`
+  width: 550px;
+  height: 950px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  border-radius: 50%;
+  transform: rotate(80deg);
+  top: -570px;
+  left: -70px;
+  background: #4E37B2;
+  background: linear-gradient(58deg, rgba(78,55,178,1) 20%, rgba(81,23,194,1) 100%);
+    `;
+    
+    const HeaderContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    `;
+    
+    const backdropVariants = {
+      expanded: {
+        width: "1650px",
+        height: "2050px",
+        // borderRadius: "20%",
+        transform: "rotate(60deg)",
+      },
+      collapsed: {
+        width: "550px",
+        height: "950px",
+        borderRadius: "50%",
+        transform: "rotate(80deg)",
+      },
+    };
+    const HeaderText = styled.h2`
+  font-size: 30px;
+  font-weight: 600;
+  line-height: 1.24;
+  color: #fff;
+  z-index: 10;
+  margin: 0;
+`;
+
+const SmallText = styled.h5`
+  color: #fff;
+  font-weight: 500;
+  font-size: 11px;
+  z-index: 10;
+  margin: 0;
+  margin-top: 7px;
+`;
+
+const InnerContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 1.8em;
+`;
+
+
+const expandingTransition = {
+  type: "spring",
+  duration: 2.3,
+  stiffness: 30,
+};
+
+export function AccountBox(props) {
+  const [isExpanded, setExpanded] = useState(false);
+  const [active, setActive] = useState("signin");
+
+  const playExpandingAnimation = () => {
+    setExpanded(true);
+    setTimeout(() => {
+      setExpanded(false);
+    }, expandingTransition.duration * 1000 - 1500);
+  };
+
+  const switchToSignup = () => {
+    playExpandingAnimation();
+    setTimeout(() => {
+      setActive("signup");
+    }, 400);
+  };
+
+  const switchToSignin = () => {
+    playExpandingAnimation();
+    setTimeout(() => {
+      setActive("signin");
+    }, 400);
+  };
+
+  const contextValue = { switchToSignup, switchToSignin };
+
+  return (
+    <AccountContext.Provider value={contextValue}>
+      <BoxContainer>
+        <TopContainer>
+          <BackDrop
+            initial={false}
+            animate={isExpanded ? "expanded" : "collapsed"}
+            variants={backdropVariants}
+            transition={expandingTransition}
+          />
+          {active === "signin" && (
+            <HeaderContainer>
+              <HeaderText>Welcome</HeaderText>
+              <HeaderText>Back</HeaderText>
+              <SmallText>Please sign-in to continue!</SmallText>
+            </HeaderContainer>
+          )}
+          {active === "signup" && (
+            <HeaderContainer>
+              <HeaderText>Create</HeaderText>
+              <HeaderText>Account</HeaderText>
+              <SmallText>Please sign-up to continue!</SmallText>
+            </HeaderContainer>
+          )}
+        </TopContainer>
+        <InnerContainer>
+          {active === "signin" && <LoginForm />}
+          {active === "signup" && <SignupForm />}
+        </InnerContainer>
+      </BoxContainer>
+    </AccountContext.Provider>
+  );
+}
